2fdad89980bff73908dfdfe8af7f409a2e3ce217
zcpisme <89170273+zcpisme@users.noreply.github.com>
2023-08-20 14:35:47 +0800

    updatetoGooglesheets

--- main.py
+++ main.py
@@ -8,7 +8,62 @@ Created on Thu Aug 17 16:40:17 2023
 import pandas as pd
 import streamlit as st
 from google.oauth2 import service_account
+from gspread_pandas import Spread,Client
+
 from gsheetsdb import connect
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Create a Google Authentication connection object
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = service_account.Credentials.from_service_account_info(
+                st.secrets["gcp_service_account"], scopes = scope)
+client = Client(scope=scope,creds=credentials)
+spreadsheetname = "application_info"
+spread = Spread(spreadsheetname,client = client)
+
+# Check the connection
+
+
+sh = client.open(spreadsheetname)
+worksheet_list = sh.worksheets()
+#st.write(worksheet_list)
+
+#@st.cache()
+# Get our worksheet names
+def worksheet_names():
+    sheet_names = []   
+    for sheet in worksheet_list:
+        sheet_names.append(sheet.title)  
+    return sheet_names
+
+# Get the sheet as dataframe
+def load_the_spreadsheet(spreadsheetname):
+    worksheet = sh.worksheet(spreadsheetname)
+    df = pd.DataFrame(worksheet.get_all_records())
+    return df
+
+# Update to Sheet
+def update_the_spreadsheet(spreadsheetname,df):
+    col = ['company_name','position','location','status',
+           'sponsorship','website']
+    spread.df_to_sheet(df[col],sheet = spreadsheetname,index = False,
+                       replace = True)
+    st.sidebar.info('Updated to GoogleSheet')
+
+
+# Check whether the sheets exists
+what_sheets = worksheet_names()
+#st.sidebar.write(what_sheets)
+ws_choice = st.sidebar.radio('Available worksheets',what_sheets)
+
+# Load data from worksheets
+df = load_the_spreadsheet(ws_choice)
+
+#st.dataframe(df)
 
 
 delete_data = False
@@ -23,7 +78,7 @@ def set_to_false():
     undo_delete_data = False
 
 st.title('Application Follow-up APP')
-df = pd.read_csv('application_info.csv')
+#df = pd.read_csv('application_info.csv')
 df = df.astype({"location": str, "sponsorship":str})
 
 # st.dataframe(df)
@@ -67,10 +122,11 @@ if add_data:
                     'website':website}
         df = pd.concat([df, pd.DataFrame([new_data])], ignore_index=True)
 #        df = df.append(new_data, ignore_index = True)
-        df.to_csv('application_info.csv', index = False)
+        #df.to_csv('application_info.csv', index = False)
+        update_the_spreadsheet('application_info',df)
         set_to_false()
     else:
-        st.write('Need Company Name/position')
+        st.sidebar.info('Need Company Name/position')
         set_to_false()
 
 existing_company = set(df['company_name'])
@@ -99,14 +155,16 @@ if delete_data:
     df.drop(index = [d1], 
             inplace = True)
     df.reset_index(drop=True,inplace = True)
-    df.to_csv('application_info.csv', index = False)
+    update_the_spreadsheet('application_info',df)
+    #df.to_csv('application_info.csv', index = False)
     set_to_false()
     
 undo_delete_data = delete_form.form_submit_button('Undo')
 if undo_delete_data:
     df = pd.read_csv('backup.csv')
     df = df.astype({"location": str})
-    df.to_csv('application_info.csv', index = False)
+    #update_the_spreadsheet('application_info',df)
+    #df.to_csv('application_info.csv', index = False)
     set_to_false()
     
 
@@ -183,37 +241,42 @@ edited_df = st.data_editor(
     },
 )
 
-edited_df.to_csv('application_info.csv', index = False)
+#st.write('split')
+#st.dataframe(edited_df)
+#update_the_spreadsheet('application_info',df)
+update_the_spreadsheet('application_info',edited_df)
+#edited_df.to_csv('application_info.csv', index = False)
 
 # st.dataframe(df)
 # st.write(existing_company)
-st.write("""Organizations marked with an asterisk (*) 
-         have several data science-related positions on their 
-         website; please visit the respective company's careers/jobs
-         website to see what interests you!""")
+# st.write("""Organizations marked with an asterisk (*) 
+#          have several data science-related positions on their 
+#          website; please visit the respective company's careers/jobs
+#          website to see what interests you!""")
 
 
 
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-conn = connect(credentials=credentials)
+# # Create a connection object.
+# credentials = service_account.Credentials.from_service_account_info(
+#     st.secrets["gcp_service_account"],
+#     scopes=[
+#         "https://www.googleapis.com/auth/spreadsheets",
+#     ],
+# )
+# conn = connect(credentials=credentials)
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_resource(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
+# # Perform SQL query on the Google Sheet.
+# # Uses st.cache_data to only rerun when the query changes or after 10 min.
+# @st.cache_resource(ttl=600)
+# def run_query(query):
+#     rows = conn.execute(query, headers=1)
+#     rows = rows.fetchall()
+#     return rows
 
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
+# sheet_url = st.secrets["private_gsheets_url"]
+# rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-# Print results.
-for row in rows:
-    st.write(f"{row.company_name} has a :{row.status}:")
+# # Print results.
+# for row in rows:
+#     st.write(f"{row.company_name} has a :{row.status}:")
+    
